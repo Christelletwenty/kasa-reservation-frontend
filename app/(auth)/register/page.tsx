642acac035f192e80df173590a718849a458a43e
Christelle Giga <christellebento@hotmail.com>
2026-04-03 12:09:24 +0200

    feat:add register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,74 @@
+"use client";
+import styles from "./RegisterPage.module.css";
+
 export default function RegisterPage() {
-  return <div>Register page</div>;
+  return (
+    <div className={styles.register__page}>
+      <h1>Rejoignez la communauté Kasa</h1>
+      <p>
+        Créez votre compte et commencez à voyager autrement : réservez des
+        logements uniques, découvrez de nouvelles destinations et partagez vos
+        propres lieux avec d’autres voyageurs.
+      </p>
+      <div className={styles.register__form__container}>
+        <form className={styles.register__form}>
+          <label className={styles.register__label} htmlFor="name">
+            Nom
+          </label>
+          <input
+            className={styles.register__input}
+            type="name"
+            id="name"
+            required
+          />
+          <label className={styles.register__label} htmlFor="prenom">
+            Prénom
+          </label>
+          <input
+            className={styles.register__input}
+            type="prenom"
+            id="prenom"
+            required
+          />
+          <label htmlFor="email" className={styles.register__label}>
+            Adresse email
+          </label>
+          <input
+            className={styles.register__input}
+            type="email"
+            id="email"
+            required
+          />
+          <label htmlFor="password" className={styles.register__label}>
+            Mot de passe
+          </label>
+          <input
+            className={styles.register__input}
+            type="password"
+            id="password"
+            required
+          />
+
+          <label className={styles.register__conditions}>
+            <input type="checkbox" required />
+            J'accepte les {""}
+            <a href="/conditions">conditions générales d'utilisation</a>
+          </label>
+
+          <button type="submit" className={styles.register__button}>
+            S'inscrire
+          </button>
+          <a className={styles.register__link} href="#">
+            Mot de passe oublié
+          </a>
+        </form>
+        <p className={styles.register__footer}>
+          Déjà membre ?{" "}
+          <a className={styles.register__link} href="/login">
+            Se connecter
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
